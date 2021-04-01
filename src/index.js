7aeff8fc79a939e0a7b6a50141e51ac4d9dea567
umutakin <umutakin@fastmail.com>
2021-04-01 22:35:30 +0300

    phaserjs udemy 017 - place pipes

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ const config = {
     default: 'arcade',
     arcade: {
       debug: true,
-      gravity: {
-        y: 400
-      }
+      // gravity: {
+      //   y: 400
+      // }
     }
   },
   scene: {
@@ -30,6 +30,7 @@ function preload() {
 
   this.load.image('sky', 'assets/sky.png');
   this.load.image('bird', 'assets/bird.png');
+  this.load.image('pipe', 'assets/pipe.png');
 }
 
 const STARTING_POSITION = {
@@ -40,7 +41,8 @@ const VELOCITY = 200;
 const VELOCITY_FLAP = 300;
 
 let bird = null;
-let totalDelta = null;
+let upperPipe = null;
+let lowerPipe = null;
 
 // Initialize Application
 function create() {
@@ -48,12 +50,32 @@ function create() {
   this.add.image(0, 0, 'sky').setOrigin(0, 0);
   
   bird = this.physics.add.sprite(STARTING_POSITION.X, STARTING_POSITION.Y, 'bird').setOrigin(0, 0);
+  bird.body.gravity.y = 400;
+
+  //drawPipe(this, 300, 200);
+  //drawPipe(this, 500, 150);
+  upperPipe = this.physics.add.sprite(400, 100, 'pipe').setOrigin(0, 1);
+  lowerPipe = this.physics.add.sprite(400, upperPipe.y + 100, 'pipe').setOrigin(0, 0);
+
+  //this.add.image(200, 0, 'pipe').setOrigin(0, 0);
+
+  //pipe = this.physics.add.sprite(STARTING_POSITION.X, STARTING_POSITION.Y, 'pipe');
 
   this.input.on('pointerdown', flap);
-
   this.input.keyboard.on('keydown-SPACE', flap);
 
 }
+
+// function drawPipe(canvas, x, y) {
+//   // canvas.physics.add.sprite(x, y - 575, 'pipe').setOrigin(0, 0);
+//   // canvas.physics.add.sprite(x, y + 25, 'pipe').setOrigin(0, 0);
+//   // pipe = this.physics.add.sprite(300, 100, 'pipe').setOrigin(0, 0);
+
+//   // upper pipe
+//   canvas.physics.add.sprite(x, y, 'pipe').setOrigin(0, 1);
+//   // lower pipe
+//   canvas.physics.add.sprite(x, y + 100, 'pipe').setOrigin(0, 0);
+// }
 
 // if bird y position is small than 0 or greater than height of the canvas
 // the alert "you have lost"
