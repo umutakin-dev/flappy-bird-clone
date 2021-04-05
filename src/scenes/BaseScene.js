84d0da1570031da4b7bda970ffe0363cfc458f51
umutakin <umutakin@fastmail.com>
2021-04-05 23:35:17 +0300

    phaserjs 02 041 menu items

--- src/scenes/BaseScene.js
+++ src/scenes/BaseScene.js
@@ -5,6 +5,10 @@ class BaseScene extends Phaser.Scene {
     constructor(key, config) {
         super(key);
         this.config = config;
+        this.screenCenter = [config.width / 2, config.height / 2]
+        this.fontSize = 32;
+        this.lineHeight = 42;
+        this.fontOptions = {fontSize: `${this.fontSize}px`, fill: '#CD00FF'}
     }
 
     create() {
@@ -13,6 +17,21 @@ class BaseScene extends Phaser.Scene {
 
     createBG() {
         this.add.image(0, 0, 'sky').setOrigin(0, 0);
+    }
+
+    createMenu(menu) {
+        let lastMenuPositionY = 0;
+        menu.forEach(menuItem => {
+            const menuPosition = [
+                this.screenCenter[0], 
+                this.screenCenter[1] + lastMenuPositionY];
+            this.add.text(
+                ...menuPosition, 
+                menuItem.text, 
+                this.fontOptions)
+                .setOrigin(0.5, 1);
+            lastMenuPositionY += this.lineHeight;
+        })
     }
 
 }
