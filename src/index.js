dbf9a5481e3526f179817c1884dcdb6576088dc8
umutakin <umutakin@fastmail.com>
2021-04-05 23:04:33 +0300

    phaserjs 02 039 optimization in scenes

--- src/index.js
+++ src/index.js
@@ -13,6 +13,15 @@ const SHARED_CONFIG = {
   startPosition: BIRD_POSITION
 }
 
+const Scenes = [
+  PreloadScene,
+  MenuScene,
+  PlayScene
+];
+
+const createScene = Scene => new Scene(SHARED_CONFIG);
+const initScenes = () => Scenes.map(createScene);
+
 const config = {
   type: Phaser.AUTO,
   ...SHARED_CONFIG,
@@ -22,7 +31,7 @@ const config = {
       debug: true,
     }
   },
-  scene: [PreloadScene, new MenuScene(SHARED_CONFIG), new PlayScene(SHARED_CONFIG)]
+  scene: initScenes()
 }
 
 new Phaser.Game(config);
